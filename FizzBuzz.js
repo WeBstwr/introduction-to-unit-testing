ebafa675ea7cf93dae4d690c417a0851cd1bd823
WeBstwr <ifedhawebster@gmail.com>
2024-05-27 16:32:19 +0300

    test the fizzbuzz question

---
+++ FizzBuzz.js
@@ -0,0 +1,28 @@
+`
+Write a function that will take a number
+If the number is divisible by 3, it returns the string "Fizz"
+If the number is divisible by 5, it returns the string "Buzz"
+If the number is divisible by both 3 and 5, it returns the string "FizzBuzz"
+Otherwise if the number is not divisible by 3 or 5, then it returns the number itself.
+`
+
+function fizzBuzz(num) {
+     if (num % 3 == 0 && num % 5 == 0) {
+      return "FizzBuzz";
+    }
+    else if (num % 3 == 0) {
+        return "Fizz"
+    }
+    else if (num % 5 == 0) {
+        return "Buzz";
+    }
+    else {
+      return num;
+    }
+}
+  
+  for (let num = 1; num <= 100; num++) {
+    console.log(fizzBuzz(num))
+}
+
+  module.exports = {fizzBuzz};git
---
+++ FizzBuzz.test.js
@@ -0,0 +1,19 @@
+const {fizzBuzz} = require("./FizzBuzz")
+
+describe("fizzBuzz", () =>{ 
+    it("returns 'Fizz' if num is divisible by 3", () =>{
+        expect(fizzBuzz(3)).toBe('Fizz');
+    })
+
+    it("returns 'Buzz' if num is divisible by 5", () =>{
+        expect(fizzBuzz(5)).toBe('Buzz');
+    })
+
+    it("returns 'FizzBuzz' if num is divisible by both 3 and 5", () =>{
+        expect(fizzBuzz(15)).toBe('FizzBuzz');
+    })
+
+    it("returns num if num is not divisible by 3 or 5", () =>{
+        expect(fizzBuzz(98)).toBe(98);
+    })
+})
